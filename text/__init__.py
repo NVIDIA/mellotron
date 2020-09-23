f883292807050f0b4c55a44638445999b9e335da
Jeevesh Juneja <creativityinczenyoga@gmail.com>
2020-09-23 08:32:03 +0530

    Corrected 'cmudict' to 'dictionary' in __init__.py

--- text/__init__.py
+++ text/__init__.py
@@ -43,7 +43,7 @@ def text_to_sequence(text, cleaner_names, dictionary=None, p_arpabet=1.0):
     m = _curly_re.match(text)
     if not m:
       clean_text = _clean_text(text, cleaner_names)
-      if cmudict is not None:
+      if dictionary is not None:
         words = _words_re.findall(text)
         clean_text = [
           get_arpabet(word[0], dictionary)
